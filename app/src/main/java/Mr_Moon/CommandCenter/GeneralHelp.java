23842c82948ead8efe5ac60abb66d3cde122055c
503-User-Unavailable <99623066+503-User-Unavailable@users.noreply.github.com>
2022-04-05 19:32:35 -0400

    took out auto generated stubs

--- app/src/main/java/Mr_Moon/CommandCenter/GeneralHelp.java
+++ app/src/main/java/Mr_Moon/CommandCenter/GeneralHelp.java
@@ -12,7 +12,6 @@ public class GeneralHelp implements CommandInterface{
 
     @Override
     public String help(String prefix) {
-        // TODO Auto-generated method stub
         return "```General Commands```";
     }
     
