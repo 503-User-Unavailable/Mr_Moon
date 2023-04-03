43c8914d6baf5946745963991c5699f9de879239
503-User-Unavailable <99623066+503-User-Unavailable@users.noreply.github.com>
2023-04-03 01:20:05 -0400

    Greatly simplified PlayerManager's code structure

--- app/src/main/java/Mr_Moon/PlayerManager.java
+++ app/src/main/java/Mr_Moon/PlayerManager.java
@@ -54,38 +54,9 @@ public class PlayerManager {
             //adding a single track [LINKS ONLY, YOUTUBE SEARCHES ARE CONSIDERED PLAYLISTS]
             @Override
             public void trackLoaded(AudioTrack track) {
+                createEmbed(channel, trackUrl, event, musicManager, track, null);
                 //inputing the member data in track for "grab" and "now playing" commands
                 track.setUserData(event.getMember());
-                
-                String[] URLSplit = track.getInfo().uri.split("=");
-                String URL = "https://i.ytimg.com/vi/" + URLSplit[1] + "/default.jpg";
-                GuildMusicManager guildMusicManager = PlayerManager.getInstance().getMusicManager(event.getGuild());
-                AudioPlayer Audioplayer = guildMusicManager.audioPlayer;
-
-                //Calculating total time and making it into a formatted string
-                Long totalTime = (long) 0;
-                if (!(Audioplayer.getPlayingTrack() == null)) {totalTime += (Audioplayer.getPlayingTrack().getDuration() - Audioplayer.getPlayingTrack().getPosition());}
-                for (var elt : musicManager.scheduler.queue) {totalTime += elt.getDuration();}
-                String totalTimeString = formatTime(totalTime);
-
-                //if its empty AKA: first song played after silence
-                if (Audioplayer.getPlayingTrack() == null){
-                    channel.sendMessage("**Searching** :mag_right: `"+ trackUrl +"`\n"
-                    + "**Playing** :notes: `" + track.getInfo().title + "` - Now!").queue();
-                }
-                //not the first song after silence
-                else{
-                    EmbedBuilder embed = new EmbedBuilder()
-                        .setAuthor("Added to queue", null, event.getAuthor().getAvatarUrl())
-                        .setDescription("**[" + track.getInfo().title + "](" + track.getInfo().uri + ")**")
-                        .addField("Channel", track.getInfo().author, true)
-                        .addField("Song Duration", formatTime(track.getDuration()), true)
-                        .addField("Time Until Playing", totalTimeString, true)
-                        .setThumbnail(URL)
-                        .setColor(new Color(255,25,25))
-                        .addField("Position in queue", (musicManager.scheduler.queue.size() + 1) + "", false);
-                    channel.sendMessageEmbeds(embed.build()).queue();
-                }
                 musicManager.scheduler.queue(track);
             }
           
@@ -94,69 +65,17 @@ public class PlayerManager {
             public void playlistLoaded(AudioPlaylist playlist) {
                 final List<AudioTrack> tracks = playlist.getTracks();
                 int count = 0;
-
                 //this is for if it is an actual youtube playlist, not a search
-                if (!playlist.isSearchResult()){
-                    //prereqs
-                    GuildMusicManager guildMusicManager = PlayerManager.getInstance().getMusicManager(event.getGuild());
-                    AudioPlayer Audioplayer = guildMusicManager.audioPlayer;
-                    String queueWait = (Audioplayer.getPlayingTrack() == null) ? "Now" : (musicManager.scheduler.queue.size() + 1) + "";
-                    String[] URLSplit = playlist.getTracks().get(0).getInfo().uri.split("=");
-                    String URL = "https://i.ytimg.com/vi/" + URLSplit[1] + "/default.jpg";
-                    //time shenanigins
-                    Long totalTime = (long) 0;
-                    if (!(Audioplayer.getPlayingTrack() == null)) {totalTime += (Audioplayer.getPlayingTrack().getDuration() - Audioplayer.getPlayingTrack().getPosition());}
-                    for (var elt : musicManager.scheduler.queue) {totalTime += elt.getDuration();}
-                    String totalTimeString = formatTime(totalTime);
-
-
-                    //embed being made and sent
-                    EmbedBuilder embed2 = new EmbedBuilder()
-                        .setColor(new Color(255,25,25))
-                        .setAuthor("Added to queue", null, event.getAuthor().getAvatarUrl())
-                        .setDescription("**" + playlist.getName() + "**")
-                        .addField("Time Until Playing", totalTimeString, false)
-                        .setThumbnail(URL)
-                        .addField("Position in queue", queueWait, true)
-                        .addField("Enqueued", playlist.getTracks().size() + "", true);
-                    channel.sendMessageEmbeds(embed2.build()).queue();
+                if (!playlist.isSearchResult()) {
+                    createEmbed(channel, trackUrl, event, musicManager, null, playlist);
                 }
                 for (AudioTrack track : tracks) {
                     //always do the first one, then it'll do the rest if it isn't search results
                     if ((count == 0) || (!playlist.isSearchResult())){
                         if ((playlist.isSearchResult())) {
-                            String[] URLSplit = track.getInfo().uri.split("=");
-                            String URL = "https://i.ytimg.com/vi/" + URLSplit[1] + "/default.jpg";
-                            String searchResults = trackUrl.split("ytsearch:")[1];
-                            GuildMusicManager guildMusicManager = PlayerManager.getInstance().getMusicManager(event.getGuild());
-                            AudioPlayer Audioplayer = guildMusicManager.audioPlayer;
-
-                            //time Shenanigins
-                            Long totalTime = (long) 0;
-                            AudioTrack playingTrack = Audioplayer.getPlayingTrack();
-                            if (!(Audioplayer.getPlayingTrack() == null)) {totalTime += (playingTrack.getDuration() - playingTrack.getPosition());}
-                            for (var elt : musicManager.scheduler.queue) {totalTime += elt.getDuration();}
-                            String totalTimeString = formatTime(totalTime);
-
-                            //if its empty AKA: first song played after silence
-                            if (Audioplayer.getPlayingTrack() == null){
-                                channel.sendMessage("**Searching** :mag_right: `"+ searchResults +"`\n"
-                                + "**Playing** :notes: `" + track.getInfo().title + "` - Now!").queue();
-                            }
-                            else{
-                                EmbedBuilder embed = new EmbedBuilder()
-                                    .setAuthor("Added to queue", null, event.getAuthor().getAvatarUrl())
-                                    .setDescription("[" + track.getInfo().title + "](" + track.getInfo().uri + ")")
-                                    .addField("Channel", track.getInfo().author, true)
-                                    .addField("Song Duration", formatTime(track.getDuration()), true)
-                                    .addField("Time Until Playing", totalTimeString, true)
-                                    .setThumbnail(URL)
-                                    .addField("Position in queue", (musicManager.scheduler.queue.size() + 1) + "", false)
-                                    .setColor(new Color(255,25,25));
-                                channel.sendMessageEmbeds(embed.build()).queue();
-                            }
+                            createEmbed(channel, trackUrl, event, musicManager, track, null);
                         }
-                        //inputing the user data in with every track
+                        //inputing the member data in track for "grab" and "now playing" commands
                         track.setUserData(event.getMember());
                         musicManager.scheduler.queue(track);
                         count++;
@@ -178,13 +97,59 @@ public class PlayerManager {
         });
     }
 
+    public void createEmbed(TextChannel channel, String trackUrl, MessageReceivedEvent event, GuildMusicManager musicManager, AudioTrack track, AudioPlaylist playlist) {
+        AudioTrack toUse = playlist == null ? track : playlist.getTracks().get(0);
+        String URL = "https://i.ytimg.com/vi/" + toUse.getInfo().uri.split("=")[1] + "/default.jpg";
+        AudioPlayer Audioplayer = PlayerManager.getInstance().getMusicManager(event.getGuild()).audioPlayer;
+        String nameToBe = trackUrl.contains("ytsearch:") ? trackUrl.split("ytsearch:")[1] : trackUrl;
+        String queueWait = (Audioplayer.getPlayingTrack() == null) ? "Now" : (musicManager.scheduler.queue.size() + 1) + "";
+
+        //formatting time into convenient string
+        Long totalTime = (long) 0;
+        AudioTrack playingTrack = Audioplayer.getPlayingTrack();
+        if (!(Audioplayer.getPlayingTrack() == null)) {totalTime += (playingTrack.getDuration() - playingTrack.getPosition());}
+        for (var elt : musicManager.scheduler.queue) {totalTime += elt.getDuration();}
+        String totalTimeString = formatTime(totalTime);
+
+        //playlist, priority over starting from silence
+        if (playlist != null) {
+            EmbedBuilder embed = new EmbedBuilder()
+                .setAuthor("Added to queue", null, event.getAuthor().getAvatarUrl())
+                .setDescription("**" + playlist.getName() + "**")
+                .addField("Time Until Playing", totalTimeString, false)
+                .addField("Position in queue", queueWait, true)
+                .addField("Enqueued", playlist.getTracks().size() + "", true)
+                .setThumbnail(URL)
+                .setColor(new Color(255,25,25));
+            channel.sendMessageEmbeds(embed.build()).queue();
+        }
+        //if its empty AKA: first song played after silence
+        else if (Audioplayer.getPlayingTrack() == null) {
+            channel.sendMessage("**Searching** :mag_right: `"+ nameToBe +"`\n"
+            + "**Playing** :notes: `" + track.getInfo().title + "` - Now!").queue();
+        }
+        //search result or direct link while music is already playing
+        else {
+            EmbedBuilder embed = new EmbedBuilder()
+                .setAuthor("Added to queue", null, event.getAuthor().getAvatarUrl())
+                .setDescription("[" + track.getInfo().title + "](" + track.getInfo().uri + ")")
+                .addField("Channel", track.getInfo().author, true)
+                .addField("Song Duration", formatTime(track.getDuration()), true)
+                .addField("Time Until Playing", totalTimeString, true)
+                .addField("Position in queue", (musicManager.scheduler.queue.size() + 1) + "", false)
+                .setThumbnail(URL)
+                .setColor(new Color(255,25,25));
+            channel.sendMessageEmbeds(embed.build()).queue();
+        }
+    }
+
     //A quick method to get the instance of the currect player manager, if there isn't one, it creates one.
     public static PlayerManager getInstance() {
         if (INSTANCE == null) {INSTANCE = new PlayerManager();}
         return INSTANCE;
     }
 
-    //easy formating for time
+    //time formatting function
     private String formatTime(long timeInMillis) {
         long hours = (timeInMillis / TimeUnit.HOURS.toMillis(1));
         long minutes = (timeInMillis / TimeUnit.MINUTES.toMillis(1)) - (60 * hours);
